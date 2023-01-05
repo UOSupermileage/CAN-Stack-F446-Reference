68fcf70a8d24274f7263b0621617e25dccb5be65
Mingye <mingyechen521@gmail.com>
2023-01-05 01:00:38 -0500

    Working can stack, using polling

---
+++ Core/Src/InternalCommsModule.h
@@ -0,0 +1,21 @@
+/*
+ * InternalCommsModule.h
+ *
+ *  Created on: Aug 6, 2022
+ *      Author: mingye chen
+ */
+
+#ifndef MODULES_INTERNALCOMMSMODULE_H_
+#define MODULES_INTERNALCOMMSMODULE_H_
+
+#include "CANDriver.h"
+#include "ApplicationTypes.h"
+
+PUBLIC result_t IComms_Init();
+PUBLIC result_t IComms_ReceiveNextMessage(iCommsMessage_t * rxMsg);
+PUBLIC uint8_t IComms_HasRxMessage();
+PUBLIC void IComms_Update();
+PUBLIC result_t IComms_Transmit(iCommsMessage_t * txMsg);
+
+
+#endif /* MODULES_INTERNALCOMMSMODULE_H_ */
